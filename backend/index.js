ba28308d050e8d116d504f56401b64cecc1cf906
tung <tung.bui2004lk@hcmut.edu.vn>
2025-10-06 09:41:43 +0700

    f commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config()
+const PORT = process.env.PORT
+
+const app = express();
+app.use(express.json());
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     credentials: true
+// }));
+
+app.listen(PORT, () => {
+    console.log("Server is listening in Port: ", PORT);
+})
