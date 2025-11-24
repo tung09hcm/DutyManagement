bd0dc7fee912a866035f703b8f2ebb544e9f1897
tung <tung.bui2004lk@hcmut.edu.vn>
2025-11-24 09:47:38 +0700

    deploy

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -5,6 +5,7 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'mysql',
   logging: false, 
   timezone: '+07:00'
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3030/api',
+  baseURL: 'https://dutymanagement.onrender.com/api',
   withCredentials: true, // Include credentials in requests
 });
