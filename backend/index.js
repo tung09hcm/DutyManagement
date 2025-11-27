d82a39327dff07d27de9e97545d9af9d887600fd
tung <tung.bui2004lk@hcmut.edu.vn>
2025-11-27 12:23:39 +0700

    eddit path

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.set("trust proxy", 1);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://dutymanagement-3.onrender.com",
     // origin: "https://dutymanagement-2.onrender.com",
     credentials: true
 }));
